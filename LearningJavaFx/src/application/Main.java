b0519f70399b05dfbbf44824b50e99ff97e6e593
Kion Smith <kion_smith@hotmail.com>
2018-03-13 15:49:43 -0500

    Lines and text tests

--- LearningJavaFx/src/application/Main.java
+++ LearningJavaFx/src/application/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 
 
 public class Main extends Application {
@@ -16,19 +17,38 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try 
 		{
+			Line underTextLine = new Line();
 			Line testLine = new Line();
+			Text helloText = new Text();
 			//Button testButton = new Button(ActionEvent e);
-			Group newGroup = new Group(testLine);
+			Group newGroup = new Group(testLine,helloText,underTextLine);
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(newGroup,400,400);
 			
+			//creating the line
 			testLine.setStartX(10);
-			testLine.setStartY(10);
+			testLine.setStartY(200);
 			
 			testLine.setEndX(390);
-			testLine.setEndY(390);
+			testLine.setEndY(300);
+			
+			// line under text
+			underTextLine.setStartX(100);
+			underTextLine.setStartY(105);
+			
+			underTextLine.setEndX(250);
+			underTextLine.setEndY(105);
+			
+			
+			
+			//creating the text
+			helloText.setX(100);
+			helloText.setY(100);
+			
+			helloText.setText("Hello world, first JavaFX App");
 			
 			scene.setFill(Color.RED);
+			
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			primaryStage.setTitle("Testing in JavaFX");
