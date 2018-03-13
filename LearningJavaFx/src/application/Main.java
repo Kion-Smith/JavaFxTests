1e2e39246e9b39450f403c3bb7d0ed7101aaea50
Kion Smith <kion_smith@hotmail.com>
2018-03-13 15:42:37 -0500

    Learned about lines and stages

--- LearningJavaFx/src/application/Main.java
+++ LearningJavaFx/src/application/Main.java
@@ -1,21 +1,43 @@
 package application;
 	
+import java.awt.Button;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
+		try 
+		{
+			Line testLine = new Line();
+			//Button testButton = new Button(ActionEvent e);
+			Group newGroup = new Group(testLine);
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(newGroup,400,400);
+			
+			testLine.setStartX(10);
+			testLine.setStartY(10);
+			
+			testLine.setEndX(390);
+			testLine.setEndY(390);
+			
+			scene.setFill(Color.RED);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setTitle("Testing in JavaFX");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+			
+		}
+		catch(Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
