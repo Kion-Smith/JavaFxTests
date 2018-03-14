40b4dcd6c26a6133a03a9246fac381662e7881fb
Kion Smith <kion_smith@hotmail.com>
2018-03-14 13:54:58 -0500

    Learned about paths and moveto

--- LearningJavaFx/src/application/Main.java
+++ LearningJavaFx/src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.text.Text;
 
 
@@ -21,9 +24,9 @@ public class Main extends Application {
 			Line testLine = new Line();
 			Text helloText = new Text();
 			//Button testButton = new Button(ActionEvent e);
-			Group newGroup = new Group(testLine,helloText,underTextLine);
+			
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(newGroup,400,400);
+			
 			
 			//creating the line
 			testLine.setStartX(10);
@@ -40,7 +43,23 @@ public class Main extends Application {
 			underTextLine.setEndY(105);
 			
 			
+			Path starPath = new Path();
 			
+			//Need the moveto object
+			MoveTo move = new MoveTo(108,71);
+			
+			LineTo line1 = new LineTo(321,161);
+			LineTo line2 = new LineTo(126,232);
+			LineTo line3 = new LineTo(232,52);
+			LineTo line4 = new LineTo(269,250);
+			LineTo line5 = new LineTo(108,71);
+			
+			starPath.getElements().add(move);
+			starPath.getElements().addAll(line1,line2,line3,line4,line5);
+			
+			
+			Group newGroup = new Group(starPath,testLine,helloText,underTextLine);
+			Scene scene = new Scene(newGroup,600,300);
 			//creating the text
 			helloText.setX(100);
 			helloText.setY(100);
