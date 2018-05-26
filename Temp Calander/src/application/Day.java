5d500ecb5f7ee29b21eba2784b5f004fd191b3e6
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-05-25 21:24:39 -0500

    Some of the classes are being used incorectlly

--- Temp Calander/src/application/Day.java
+++ Temp Calander/src/application/Day.java
@@ -1,61 +1,68 @@
 package application;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 
 public class Day 
 {
-	//@FXML add later
-	//private TableView<Day> dayTable;
 	
-	public int curDay;
-	public ArrayList<String> toDoList;//might change this later to another object
+	//http://code.makery.ch/library/javafx-2-tutorial/part2/
+	private final IntegerProperty curDay;
+	public ObjectProperty<ArrayList<String>> toDoList;//might change this later to another object
 	
-	public Day()
+	public Day(int cday)
 	{
-		curDay = -1;
-		toDoList = new ArrayList<String>();
+		curDay = new SimpleIntegerProperty(cday);
 	}
+/*
 	
-	public Day(int d)
+	public Day(int cday, ArrayList<String> tdl)
 	{
-		curDay = d;
-		toDoList = new ArrayList<String>();
+		curDay = new SimpleIntegerProperty(cday);
+		toDoList = new SimpleObjectProperty(tdl);
 	}
-	
-	public void addActivity(String activity)
-	{
-		toDoList.add(activity);
-	}
-	
-	public void removeActivity(String activity)
-	{
-		toDoList.remove(activity);
-	}
-	
-	public int getDay()
-	{
-		return curDay;
-	}
-	
-	public ArrayList<String> getToDoList()
-	{
-		return toDoList;
-	}
-	
-	public void setDay(int d)
-	{
-		curDay = d;
-	}
-	
-	
+/*
 	public void setToDoList(ArrayList<String> list)
 	{
 		for(int i=0;i<list.size();i++)
 		{
-			toDoList.add(list.get(i));
+			//need to learn how to add items
 		}
 	}
-	
+	*/
+	 public int getCurDay() 
+	 {
+		 return curDay.get();
+	 }
+
+	 public void setCurDay(int cday) 
+	 {
+		 curDay.set(cday);
+	 }
+
+	 public IntegerProperty curdayProperty() 
+	 {
+		 return curDay;
+	 }
+	 
+	 public void setToDoList(ArrayList<String> list) 
+	 {
+	        toDoList.set(list);
+	 }
+
+	    public ObjectProperty<ArrayList<String>> toDoListProperty() 
+	    {
+	        return toDoList;
+	    }
+	 
+	 
 }
